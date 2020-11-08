6a43dc45379c685fac3520a97f7737199df73aab
53907dev02 <2018428979@11.com>
2020-11-08 14:36:25 +0800

    dev02 changed

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public  class Hello{
 	public static void main(String[] args){
 		for (int i = 0; i < 10 ; ++i){
-			System.out.println("dev02 say :"+i);
+			System.out.println("dev02 say this is  :"+i);
 		}	
 	}
 }
