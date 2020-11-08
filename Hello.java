f4bd5b0727b563977a37f903b82f91873f8b09ad
53907dev01 <1459555323@qq.com>
2020-11-08 17:50:56 +0800

    scm merged

--- Hello.java
+++ Hello.java
@@ -3,8 +3,8 @@ public  class Hello{
 		for (int i = 0; i < 10 ; ++i){
 			System.out.println("dev02 say this is  :"+i);
 			System.out.println("dev02 say this is  :"+i);
-			 System.out.println("dev02 say this is  :"+i);
-			 System.out.println("dev02 say this is  :"+i);
+			System.out.println("dev02 say this is  :"+i);
+			System.out.println("dev02 say this is  :"+i);
 		}	
 	}
 }
