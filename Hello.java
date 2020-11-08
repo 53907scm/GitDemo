064db133bfd8f7cd4dd38c0413a621852fad9575
53907dev02 <2018428979@qq.com>
2020-11-08 16:26:03 +0800

    dev01 commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public  class Hello{
 	public static void main(String[] args){
 		for (int i = 0; i < 10 ; ++i){
 			System.out.println("dev02 say this is  :"+i);
+			System.out.println("dev01 say this is  :"+i);
 		}	
 	}
 }
