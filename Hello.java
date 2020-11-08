f9c2bfb2e9720adf9ac29dc208d0e6a574774f19
53907scm <waniiu@126.com>
2020-11-08 14:30:37 +0800

    dev02 changed

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public  class Hello{
 	public static void main(String[] args){
 		for (int i = 0; i < 10 ; ++i){
-			System.out.println("develop :"+i);
+			System.out.println("dev02 say :"+i);
 		}	
 	}
 }
